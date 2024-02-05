b9df1185859bc43a6152a8253157c5538d826e8e
LaPapaSanta <154758801+LaPapaSanta@users.noreply.github.com>
2024-02-04 22:50:20 -0600

    Update funciones.js

--- assets/funciones.js
+++ assets/funciones.js
@@ -1,30 +1,13 @@
-// Función para mostrar u ocultar los contenedores de tarjetas según la selección en el combo box
-function mostrarProductos() {
-    var selectedProduct = document.getElementById('productSelect').value;
-    var crepasContainer = document.getElementById('crepasContainer');
-    var tortasContainer = document.getElementById('tortasContainer');
-    var sandwichContainer = document.getElementById('sandwichContainer');
+function ordenarPorWhatsApp() {
+    var producto = document.querySelector('.custom-card h3').textContent;
+    var precio = document.querySelector('.custom-card h4').textContent;
 
-    // Oculta todos los contenedores de tarjetas
-    crepasContainer.style.display = 'none';
-    omeletteContainer.style.display = 'none';
-    tortasContainer.style.display = 'none';
-    sandwichContainer.style.display = 'none';
+    var numeroWhatsApp = '+525560341556'; // Reemplaza con el número de WhatsApp deseado
+    var mensaje = '¡Hola! Me gustaría ordenar el producto: ' + producto + ', con precio: ' + precio;
 
-    // Muestra el contenedor correspondiente según la selección en el combo box
-    if (selectedProduct === "Crepas") {
-        crepasContainer.style.display = 'block';
-    } else if (selectedProduct === "Omelette") {
-        omeletteContainer.style.display = 'block';
-    } else if (selectedProduct === "Tortas") {
-        tortasContainer.style.display = 'block';
-    }  else if (selectedProduct === "Sandwich") {
-        sandwichContainer.style.display = 'block';
-    }
-}
+    // Crear el enlace de WhatsApp
+    var whatsappURL = 'https://wa.me/' + numeroWhatsApp + '?text=' + encodeURIComponent(mensaje);
 
-// Llama a la función cuando se cambia la selección del combo box
-document.getElementById('productSelect').addEventListener('change', mostrarProductos);
-
-// Llama a la función al cargar la página para mostrar el producto predeterminado
-mostrarProductos();
+    // Abrir WhatsApp en una nueva ventana
+    window.open(whatsappURL);
+  }
